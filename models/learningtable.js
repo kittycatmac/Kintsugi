429b871237e6d04bc004b02f446687519cb1102f
Alex Ulfich <auflcih@response.com>
2018-11-14 20:06:40 -0700

    got User, Learning and flashcard models to query

--- models/learningtable.js
+++ models/learningtable.js
@@ -1,9 +1,14 @@
 module.exports = function (sequelize, DataTypes) {
 	var Learning = sequelize.define('LearningSchema', {
-			learning_ID: DataTypes.INTEGER,
-			// primaryKey: true,
+			learning_ID: {
+				type: DataTypes.INTEGER,
+				primaryKey: true
+			},
 			symbol: DataTypes.STRING,
 			saying: DataTypes.STRING
+	}, {
+		timestamps: false,
+		freezeTableName: true
 	});
 	return Learning;
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var db = require('../models');
 module.exports = function (app) {
 	// Load index page
 	app.get('/', function (req, res) {
-		db.UserSchema.findAll({}).then(function (dbExamples) {
+		db.LearningSchema.findAll().then(function (dbExamples) {
 			console.log("DBEXAMPLES USERS: ", dbExamples);
 			res.render('index', {
 				msg: 'Welcome!',
@@ -14,7 +14,7 @@ module.exports = function (app) {
 
 	// Load example page and pass in an example by id
 	app.get('/example/:id', function (req, res) {
-		db.UsersSchema.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+		db.UserSchema.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
 			res.render('example', {
 				example: dbExample
 			});
@@ -26,3 +26,100 @@ module.exports = function (app) {
 		res.render('404');
 	});
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [ 
+// 	UserSchema {
+// 	dataValues: 
+// 	 { user_ID: 'test user',
+// 		 Bio: 'test info for user',
+// 		 badges: '15.0000',
+// 		 study_cards: 'cards' },
+// 	_previousDataValues: 
+// 	 { user_ID: 'test user',
+// 		 Bio: 'test info for user',
+// 		 badges: '15.0000',
+// 		 study_cards: 'cards' },
+// 	_changed: {},
+// 	_modelOptions: 
+// 	 { timestamps: false,
+// 		 validate: {},
+// 		 freezeTableName: true,
+// 		 underscored: false,
+// 		 underscoredAll: false,
+// 		 paranoid: false,
+// 		 rejectOnEmpty: false,
+// 		 whereCollection: null,
+// 		 schema: null,
+// 		 schemaDelimiter: '',
+// 		 defaultScope: {},
+// 		 scopes: [],
+// 		 indexes: [],
+// 		 name: [Object],
+// 		 omitNull: false,
+// 		 sequelize: [Object],
+// 		 hooks: {},
+// 		 uniqueKeys: {} },
+// 	_options: 
+// 	 { isNewRecord: false,
+// 		 _schema: null,
+// 		 _schemaDelimiter: '',
+// 		 raw: true,
+// 		 attributes: [Array] },
+// 	__eagerlyLoadedAssociations: [],
+// 	isNewRecord: false },
+// UserSchema {
+// 	dataValues: 
+// 	 { user_ID: 'userID',
+// 		 Bio: 'bio',
+// 		 badges: '1.0000',
+// 		 study_cards: 'study_card' },
+// 	_previousDataValues: 
+// 	 { user_ID: 'userID',
+// 		 Bio: 'bio',
+// 		 badges: '1.0000',
+// 		 study_cards: 'study_card' },
+// 	_changed: {},
+// 	_modelOptions: 
+// 	 { timestamps: false,
+// 		 validate: {},
+// 		 freezeTableName: true,
+// 		 underscored: false,
+// 		 underscoredAll: false,
+// 		 paranoid: false,
+// 		 rejectOnEmpty: false,
+// 		 whereCollection: null,
+// 		 schema: null,
+// 		 schemaDelimiter: '',
+// 		 defaultScope: {},
+// 		 scopes: [],
+// 		 indexes: [],
+// 		 name: [Object],
+// 		 omitNull: false,
+// 		 sequelize: [Object],
+// 		 hooks: {},
+// 		 uniqueKeys: {} },
+// 	_options: 
+// 	 { isNewRecord: false,
+// 		 _schema: null,
+// 		 _schemaDelimiter: '',
+// 		 raw: true,
+// 		 attributes: [Array] },
+// 	__eagerlyLoadedAssociations: [],
+// 	isNewRecord: false } ]
+
