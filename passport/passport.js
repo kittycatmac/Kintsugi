228c7ad56397b53625468ea798039b88e17061cc
Alex Ulfich <auflcih@response.com>
2018-11-17 12:26:06 -0700

    query flashcards from db to home

--- passport/passport.js
+++ passport/passport.js
@@ -15,7 +15,6 @@ module.exports = app => {
 	});
 
 	passport.deserializeUser((id, done) => {
-		console.log(id);
 		db.UserSchema.findById(id)
 				.then(user => {
 						done(null, user);
@@ -28,52 +27,22 @@ passport.use(
 		clientID: "206437889113-sioe43rlfeon8iocfbcnh50fcg8jme9d.apps.googleusercontent.com",
 		clientSecret: "Sx9AFgqBY96dIztmsbndoS7I",
 		callbackURL: "http://localhost:3000/auth/google/callback"
-  }, async (accessToken, refreshToken, profile, done) => {
-		console.log("PROFILE: ", profile);
+  }, async (accessToken, refreshToken, profile, done) => {	
+		const existingUser = await db.UserSchema.findById(profile.id );
+	
+		if(existingUser) {
+			return done(null, existingUser);
+		};
+
 		const new_user = {
 			user_ID: profile.id,
 			Bio: '',
-			badges: '1',
+			badges: '',
 			study_cards: ''
-		}
-
-		const existingUser = await db.UserSchema.findById(profile.id );
-		
-		console.log("existingUser: ", existingUser);
-		if(existingUser) {
-			    return done(null, existingUser);
-			};
+		};
 
 		const user = await db.UserSchema.create(new_user).then(user => user.dataValues);
-		console.log("USER: ", user);
 		return done(null, user);
-			
-
 	})
 	);
 }
-
-
-
-// User.create({ username: 'fnord', job: 'omnomnom' })
-//   .then(() => User.findOrCreate({where: {username: 'fnord'}, defaults: {job: 'something else'}}))
-//   .spread((user, created) => {
-//     console.log(user.get({
-//       plain: true
-//     }))
-//     console.log(created)
-
-//     /*
-//     In this example, findOrCreate returns an array like this:
-//     [ {
-//         username: 'fnord',
-//         job: 'omnomnom',
-//         id: 2,
-//         createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
-//         updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
-//       },
-//       false
-//     ]
-//     The array returned by findOrCreate gets spread into its 2 parts by the "spread" on line 69, and the parts will be passed as 2 arguments to the callback function beginning on line 69, which will then treat them as "user" and "created" in this case. (So "user" will be the object from index 0 of the returned array and "created" will equal "false".)
-//     */
-//   })
